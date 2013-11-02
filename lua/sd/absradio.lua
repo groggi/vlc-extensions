5e61fef3c2f8cfff2f4e0b1b4eec0dcbac5ba189
Gregor Wegberg <github@gregorwegberg.com>
2013-11-02 19:20:46 +0100

    code is more shiny now

--- lua/sd/absradio.lua
+++ lua/sd/absradio.lua
@@ -1,4 +1,5 @@
 local base_url = "http://icecast.timlradio.co.uk"
+local regex_value = ">([^<]*)"
 
 function descriptor()
     return { title="Absolute Radio" }
@@ -21,18 +22,18 @@ function main()
             last_element["path"] = base_url .. string.match(line, "<a href=\"([^\"]*)\">XSPF</a>")
         elseif string.match(line, "Stream Description:") then
             line = page:readline()
-            last_element["title"] = string.match(line, ">([^<]*)")
+            last_element["title"] = string.match(line, regex_value)
         elseif string.match(line, "Bitrate") then
             line = page:readline()
-            last_element["title"] = last_element["title"] .. " | Bitrate: " .. string.match(line, ">([^<]*)")
+            last_element["title"] = last_element["title"] .. " | Bitrate: " .. string.match(line, regex_value)
         elseif string.match(line, "Content Type") then
             line = page:readline()
-            last_element["title"] = last_element["title"] .. " | Type: " .. string.match(line, ">([^<]*)")
+            last_element["title"] = last_element["title"] .. " | Type: " .. string.match(line, regex_value)
         elseif string.match(line, "Stream Genre") then
             line = page:readline()
-            last_element["category"] = string.match(line, ">([^<]*)")
+            last_element["category"] = string.match(line, regex_value)
         end
-        
+
         line = page:readline()
     end
 end
